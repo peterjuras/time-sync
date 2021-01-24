74fdbb4a4317bb1f004c6125b9342696a6f0a348
Peter Juras <me@peterjuras.com>
2021-01-24 18:03:00 +0100

    chore: update coat template

--- package.json-custom.js
+++ package.json-custom.js
@@ -1,10 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = ({ files, ...packageJsonProps }) => {
-  return {
-    ...packageJsonProps,
-    scripts: {
-      ...packageJsonProps.scripts,
-      prepare: "node scripts/prepare",
-    },
-  };
+  return packageJsonProps;
 };
